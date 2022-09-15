bcfb4c74f22d17b16c9f10b569c67630dfa3b0eb
Victor Rocha <victorrochadev0@gmail.com>
2022-09-14 23:57:47 -0300

    Separação dos components do html, baixado a dependencia do Axios, Criado a API com json-server

---
+++ src/components/TodoItems.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- Todo items -->
+  <div class="space-y-2">
+    <TodoItem />
+    <TodoItem />
+    <TodoItem />
+    <TodoItem />
+  </div>
+  <!--/ Todo items -->
+</template>
+
+<script>
+import TodoItem from "./TodoItem.vue";
+export default {
+  components: { TodoItem },
+};
+</script>
